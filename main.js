b98c966ab071e4caccc49b12ee7f0cf58022e6ca
April Nichols <nicholsar@gmail.com>
2018-03-30 12:48:56 -0500

    1st event listener

--- main.js
+++ main.js
@@ -6,15 +6,41 @@ const buildCardString = (planetArray) => {
     let cardString = '';
     planetArray.forEach((planet) => {
         cardString += `<div class="planet-card">`;
-        cardString +=   `<h1>${planet.name}</h1>`;
+        cardString +=   `<h1 class="planet-name">${planet.name}</h1>`;
+        cardString +=   `<img class="planet-pic hidden" src="${planet.imageURL}">`;
         cardString += `</div>`;
     });
     printToDom(cardString, 'planet-holder');
 }
 
+const planetChosen = (e) => {
+    let planetPicked = e.target;
+    e.target.classList.add('hidden');
+    e.target.parentNode.children[1].classList.remove('hidden');
+    
+}
+
+const planetHoverEventListener = () => {
+    const planetName = document.getElementsByClassName('planet-name');
+    for (let i=0; i<planetName.length; i++) {
+        planetName[i].addEventListener('mouseover', planetChosen);
+    }
+}
+
+
+
+const showPic = () => {
+
+}
+
+const showInfo = () => {
+
+}
+
 function onLoad() {
     const data = JSON.parse(this.responseText);
     buildCardString(data.planets);
+    planetHoverEventListener(data.planets);
 }
 
 function ifFail() {
